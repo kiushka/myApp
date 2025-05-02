650167e5d0808028e31d8aa07643bdc238fb4a4a
Kayvan Ahmadi <kayvan7736@gmail.com>
2025-05-02 20:38:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # صفحه HTML فروشگاه
+    return render_template("index.html")  # فایل باید در پوشه templates باشه
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 5000))  # گرفتن پورت از متغیر محیطی
+    app.run(host='0.0.0.0', port=port)       # گوش دادن روی همه اینترفیس‌ها
