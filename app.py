b0a298cabeb77a61f7bdc13ccdc1a20d36efe468
Kayvan Ahmadi <kayvan7736@gmail.com>
2025-05-02 20:42:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
     return render_template("index.html")  # فایل باید در پوشه templates باشه
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # گرفتن پورت از متغیر محیطی
+    port = int(os.environ.get('PORT', 4000))  # گرفتن پورت از متغیر محیطی
     app.run(host='0.0.0.0', port=port)       # گوش دادن روی همه اینترفیس‌ها
