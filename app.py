08c609ddcbc021d832ea0e790ffeebf7a1b85356
Kayvan Ahmadi <kayvan7736@gmail.com>
2025-05-02 20:58:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 4000))  # گرفتن پورت از متغیر محیطی
-    app.run(host='0.0.0.0', port=port)       # گوش دادن روی همه اینترفیس‌ها
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
