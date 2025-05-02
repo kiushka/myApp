d09b0702b357e0cb4bb8c7404f531fa08518c025
Kayvan Ahmadi <kayvan7736@gmail.com>
2025-05-02 18:11:58 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # صفحه HTML فروشگاه
+
+if __name__ == '__main__':
+    app.run()
